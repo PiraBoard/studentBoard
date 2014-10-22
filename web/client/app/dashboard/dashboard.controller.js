cb1144dd91e82de5c783c6845353f9544149db40
Michael Pinter <mpinter09@gmail.com>
2014-10-21 20:49:44 -0700

    'Dashboard skeleton complete'

---
+++ web/client/app/dashboard/dashboard.controller.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('piraBoardApp')
+  .controller('DashboardCtrl', function ($scope, $http, Auth) {
+    $scope.awesomeThings = [];
+    $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.numGroups = 0;
+    $scope.numLead = 0;
+
+
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+    $scope.addThing = function() {
+      if($scope.newThing === '') {
+        return;
+      }
+      $http.post('/api/things', { name: $scope.newThing });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+  });
