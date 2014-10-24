e1b6c0ae203310745d8e4eec5d0f9f016a89d664
mikephoran <mikephoran@gmail.com>
2014-10-23 18:06:42 -0700

    Fixed a typo.

--- web/server/api/user/user.model.js
+++ web/server/api/user/user.model.js
@@ -7,7 +7,7 @@ var authTypes = ['github', 'twitter', 'facebook', 'google'];
 
 var UserSchema = new Schema({
   name: String,
-  email: { type: String, lowercase: true, required: true },
+  email: { type: String, lowercase: true},
   role: {
     type: String,
     default: 'user'
